c8e4b3721953f383dcf45fa75fcab9cc4771c1a9
Shankar Weldmesh Pvt. Ltd <77322849+ManitKedia@users.noreply.github.com>
2021-09-24 14:42:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-.
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(600, 550);
+
+    video = createCapture(550, 500);
+    video.size(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background('#969A97');
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is Loaded');
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
